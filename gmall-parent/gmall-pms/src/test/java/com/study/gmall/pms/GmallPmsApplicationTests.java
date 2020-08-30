84884b105949f14efe02eda0b7ed2d04a52a8b92
zhangsan <353131603@qq.com>
2020-08-30 22:38:58 +0800

    gmall-pms模块提交

--- gmall-parent/gmall-pms/src/test/java/com/study/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-parent/gmall-pms/src/test/java/com/study/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.study.gmall.pms;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.study.gmall.pms.entity.Brand;
+import com.study.gmall.pms.entity.Product;
+import com.study.gmall.pms.service.BrandService;
+import com.study.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class GmallPmsApplicationTests {
+public class GmallPmsApplicationTests {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        Product product = productService.getById(1);
+        System.out.println(product.getName());
+
+        //测试增删改在主库，查在从库
+        Brand brand = new Brand();
+        brand.setName("huawei");
+        brandService.save(brand);
+        System.out.println("保存成功");
     }
 
 }
